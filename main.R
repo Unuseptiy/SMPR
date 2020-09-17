f1d38fd73576918ac778c42c7f72548ac3b7487e
Valeriy K <65853387+Unuseptiy@users.noreply.github.com>
2020-09-18 00:01:41 +0300

    Update main.R

--- main.R
+++ main.R
@@ -28,22 +28,3 @@ colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
 plot(iris[,3:4], pch = 21, bg = colors[iris$Species],col = colors[iris$Species])
 
 points(z[1], z[2], pch = 22, bg = colors[class], asp = 1)
-
-
-
-
-
-
-
-#k <- matrix(NA, 10, 2)
-#for (i in 1:10) {
-#  k[i,] <- c(i, -i);
-#}
-#d <- vector(, 0)
-#for(i in 1:10) {
-#  d[i] <- 11 - i
-#}
-#
-#k[order(k[,2]), ]
-
-
