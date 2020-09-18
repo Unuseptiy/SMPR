b3abcd4a15add2ea47a811676704b40303a6b3dd
Valeriy K <65853387+Unuseptiy@users.noreply.github.com>
2020-09-18 18:41:04 +0300

    Update main.R

--- main.R
+++ main.R
@@ -1,4 +1,3 @@
-
 #функцция метрики
 EM <- function(u, v) {
   return(sqrt(sum((u - v) ^ 2)));
@@ -16,18 +15,16 @@ dist <- function(z, xl, metricFunction = EM) {
 oneNN <- function(z, xl) {
   distance <- dist(z, xl, )
   class <- iris[order(distance[,2])[1], 5] #сортируем массив расстояний и получаем класс классифицируемого объекта
-  return(class) 
+  return(class)
 }
 
 #отрисовка ирисов Фишера
 colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
 plot(iris[,3:4], pch = 21, bg = colors[iris$Species],col = colors[iris$Species])
 
+#задание 10 рандомных классифицируемых объектов и их отрисовка
 for(i in 1:10){
-  z <- c(runif(1, 0, 2.5), runif(1, 0, 2.5))
-  oneNN(z, iris[,3:4])
+  z <- c(runif(1, 0, 7), runif(1, 0, 2.5))
   #рисуем классифицируемый объект
-  points(z[1], z[2], pch = 22, bg = colors[class], asp = 1)
+  points(z[1], z[2], pch = 22, bg = colors[oneNN(z, iris[,3:4])], asp = 2)
 }
-
-
