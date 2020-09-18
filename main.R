72c9ef7b23c9201362cb285131a2ccd171c5a9ae
Valeriy K <65853387+Unuseptiy@users.noreply.github.com>
2020-09-18 11:14:34 +0300

    Update main.R

--- main.R
+++ main.R
@@ -1,21 +1,35 @@
+
 #функцция метрики
 EM <- function(u, v) {
   return(sqrt(sum((u - v) ^ 2)));
 }
 
 #функция, возвращающая массив расстояний от классифицируемого объекта до элементов обучающей выборки
-dist <- function(z, metricFunction = EM) {
-  distances <- matrix(NA, length(iris[,1]), 2)
-  for (i in 1:length(iris[,1])) {
-    distances[i,] <- c(i, metricFunction(iris[i,3:4], z))
+dist <- function(z, xl, metricFunction = EM) {
+  distances <- matrix(NA, length(xl[,1]), 2)
+  for (i in 1:length(xl[,1])) {
+    distances[i,] <- c(i, metricFunction(xl[i,1:2], z))
   }
   return(distances)
 }
 
+oneNN <- function(z, xl) {
+  distance <- dist(z, xl, )
+  class <- iris[order(distance[,2])[1], 5] #сортируем массив расстояний и получаем класс классифицируемого объекта
+  
+  #отрисовка ирисов Фишера
+  colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
+  plot(iris[,3:4], pch = 21, bg = colors[iris$Species],col = colors[iris$Species])
+  
+  #рисуем классифицируемый объект
+  points(z[1], z[2], pch = 22, bg = colors[class], asp = 1)
+}
+
 #Работа алгоритма 1нн
-z <- c(2.5, 0.7);
-distance <- dist(z,)
-class <- iris[order(distance[,2])[1], 5] #сортируем массив расстояний и получаем класс классифицирумого объекта
+z <- c(6.5, 2.5);
+xl <- iris[, 3:4]
+distance <- dist(z, xl, )
+class <- iris[order(distance[,2])[1], 5] #сортируем массив расстояний и получаем класс классифицируемого объекта
 class;
 
 #отрисовка ирисов Фишера
@@ -24,3 +38,4 @@ plot(iris[,3:4], pch = 21, bg = colors[iris$Species],col = colors[iris$Species])
 
 #рисуем классифицируемый объект
 points(z[1], z[2], pch = 22, bg = colors[class], asp = 1)
+
