8f6a1c3109f1296f60883f799fda4989fe77a3ab
Valeriy K <65853387+Unuseptiy@users.noreply.github.com>
2020-10-16 02:12:15 +0300

    Add files via upload

--- parzen.R
+++ parzen.R
@@ -1,13 +1,31 @@
+#epan - 0.4
+#quar - 0.4
+#tria - 0.4
+#gaus - 0.4
+#rect - 0.4
+
 #метрика
 EM <- function (u, v) {
   return(sqrt(sum((u - v) ^ 2)))
 }
 
-epac <- function (x) {
+#функция, возвращающая отсортированный массив расстояний от классифицируемого объекта до элементов обучающей выборки
+ruler <- function(z, feature_matrix, metric_function = EM) {
+  l <- dim(feature_matrix)[1]
+  n <- dim(feature_matrix)[2]
+  distances <- matrix(NA, length(feature_matrix[,1]), 2)
+  for(i in 1:l) {
+    distances[i,] <- c(i, metric_function(feature_matrix[i,1:n], z))
+  }
+  distances <- distances[order(distances[,2]),]
+  return(distances)
+}
+
+epan <- function (x) {
   return (3/4 * (1 - x ^ 2) * (abs(x) <= 1))
 }
 
-quad <- function (x) {
+quar <- function (x) {
   return (15/16 * (1 - x ^ 2) ^ 2 * (abs(x) <= 1))
 }
 
@@ -23,18 +41,6 @@ rect <- function (x) {
   return (1/2 * (abs(x) <= 1))
 }
 
-test <- function(algo){
-  x = -3
-  deltaX = 0.1
-  coords <- matrix(0, 0, 2)
-  while(x <= 3) {
-    coords <- rbind(coords, c(x, algo(x)))
-    x <- x + deltaX
-  }
-  plot (coords, type = "l", xlab = "x", ylab = "y")
-}
-
-
 parzen <- function (feature_matrix, labels, z, h, core) {
   l <- dim(feature_matrix)[1]
   n <- dim(feature_matrix)[2]
@@ -42,22 +48,47 @@ parzen <- function (feature_matrix, labels, z, h, core) {
   for (i in 1:l) {
     cnt[labels[i]] <- cnt[labels[i]] + core(EM(feature_matrix[i,1:n], z) / h)
   }
-  return(which.max(cnt))
+  if(cnt["setosa"] != 0 | cnt["versicolor"] != 0 | cnt["virginica"] != 0){
+    return(which.max(cnt))
+  }
 }
 
-#отрисовка ирисов Фишера
-colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
-plot(iris[,3:4], pch = 21, bg = colors[iris$Species],col = colors[iris$Species])
+parzen_LOO <- function(feature_matrix, labels, parametr_min_value, parametr_max_value, core, plot_flag = FALSE) {
+  l <- dim(feature_matrix)[1]
+  delta <- 0.1
+  rows <- (parametr_max_value - parametr_min_value) / delta + 1
+  loo <- matrix(0, ceiling(rows), 2)
+  h <- parametr_min_value
+  for (index in 1:rows) {
+    loo[index, 1] <- h
+    h <- h + delta
+  }
 
-#задание 10 рандомных классифицируемых объектов и их отрисовка
-for(i in 1:10){
-  z <- c(runif(1, 0, 7), runif(1, 0, 2.5))
-  #рисуем классифицируемый объект
-  points(z[1], z[2], pch = 22, bg = colors[parzen(iris[,3:4], iris[,5], z, h, rect)], asp = 2)
+  for (i in 1:l) {
+    tmp_feature_matrix <- feature_matrix[-i,]
+    tmp_labels <- labels[-i]
+    h <- parametr_min_value
+    distances <- ruler(feature_matrix[i,], tmp_feature_matrix)
+    for (index in 1:rows) {
+      cnt <- c("setosa" = 0, "versicolor" = 0, "virginca" = 0)
+      j <- 1
+      while(distances[j,2] <= h) {
+        cnt[tmp_labels[distances[j,1]]] <- cnt[tmp_labels[distances[j,1]]] + core(distances[j, 2] / h)
+        j <- j + 1
+      }
+      if (as.integer(which.max(cnt)) != as.integer(labels[i])){
+            loo[index, 2] <- loo[index, 2] + 1
+        }
+      h <- h + delta
+    }
+  }
+
+  if(plot_flag) {
+    plot(loo[,1], loo[,2], xlab = "h", ylab = "loo", type = "l")
+    points(which.min(loo), loo[which.min(loo)], pch = 20)
+    location <- "topright"
+    leg <- paste0("Риск минимален (количество\nошибок: ", as.character(loo[which.min(loo[,2]), 2]), ") при: h = ", as.character(loo[which.min(loo[,2]), 1]))
+    legend(location, legend = leg)
+  }
+  return(loo[which.min(loo[,2]), 1])
 }
-
-
-#z <- c(runif(1, iris$Petal.Width), runif(1, iris$Petal.Length))
-#h <- 2
-#points(z[1], z[2], pch = 22, bg = colors[parzen(iris[,3:4], iris[,5], z, h, rect)], asp = 2)
-
